25904a2d3f2a277d3ca9d80585178220aa92ccba
StereoFlo <StereoFlo@users.noreply.github.com>
2019-03-05 20:46:58 +0300

    user entity

---
+++ src/Domain/Shared/Entity/AbstractEntity.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Domain\Shared\Entity;
+
+use DateTime;
+use Exception;
+
+/**
+ * Class AbstractEntity
+ * @package Domain\Shared\Entity
+ */
+abstract class AbstractEntity
+{
+    /**
+     * @var DateTime
+     */
+    protected $createdAt;
+    /**
+     * @var DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * @return static
+     */
+    public static function create()
+    {
+        return new static();
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     *
+     * @return static
+     * @throws Exception
+     */
+    public function setCreatedAt(DateTime $createdAt = null)
+    {
+        if (empty($createdAt)) {
+            $this->createdAt = new DateTime();
+            return $this;
+        }
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     *
+     * @return static
+     * @throws Exception
+     */
+    public function setUpdatedAt(DateTime $updatedAt = null)
+    {
+        if (empty($updatedAt)) {
+            $this->updatedAt = new DateTime();
+            return $this;
+        }
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+}
---
+++ src/Domain/User/Entity/User.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Domain\User\Entity;
+
+use Domain\Shared\Entity\AbstractEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class User
+ * @package Domain\User\Entity
+ */
+class User extends AbstractEntity implements UserInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $role;
+
+    /**
+     * just a hash
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @return array
+     */
+    public function getRoles(): array
+    {
+        return [$this->role];
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername(): string
+    {
+        return $this->getEmail();
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+}
