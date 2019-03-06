d48d86d1c990c65c3bdda88401cc25df6d1bdbda
Evgeniy <evgeniy.bronnikov@ovcenter.com>
2019-03-06 18:54:54 +0300

    schema fix

---
+++ src/Domain/Training/Entity/TrainingUser.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Domain\Training\Entity;
+
+use Domain\Shared\Entity\AbstractEntity;
+use Domain\User\Entity\User;
+
+/**
+ * Class TrainigUser
+ * @package Domain\Training\Entity
+ */
+class TrainingUser extends AbstractEntity
+{
+    const TYPE_EMAIL = 0;
+    const TYPE_SMS = 1;
+
+    /**
+     * @var int
+     */
+    private $userId;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var int
+     */
+    private $trainingId;
+
+    /**
+     * @var Training
+     */
+    private $training;
+
+    /**
+     * @var int
+     */
+    private $subscriptionType;
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     *
+     * @return TrainingUser
+     */
+    public function setUserId(int $userId): TrainingUser
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return TrainingUser
+     */
+    public function setUser(User $user): TrainingUser
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTrainingId(): int
+    {
+        return $this->trainingId;
+    }
+
+    /**
+     * @param int $trainingId
+     *
+     * @return TrainingUser
+     */
+    public function setTrainingId(int $trainingId): TrainingUser
+    {
+        $this->trainingId = $trainingId;
+        return $this;
+    }
+
+    /**
+     * @return Training
+     */
+    public function getTraining(): Training
+    {
+        return $this->training;
+    }
+
+    /**
+     * @param Training $training
+     *
+     * @return TrainingUser
+     */
+    public function setTraining(Training $training): TrainingUser
+    {
+        $this->training = $training;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSubscriptionType(): int
+    {
+        return $this->subscriptionType;
+    }
+
+    /**
+     * @param int $subscriptionType
+     *
+     * @return TrainingUser
+     */
+    public function setSubscriptionType(int $subscriptionType): TrainingUser
+    {
+        $this->subscriptionType = $subscriptionType;
+        return $this;
+    }
+}
