a31b3e1b58f11b4e1b273f7779871747023548c0
StereoFlo <StereoFlo@users.noreply.github.com>
2019-03-09 18:07:20 +0300

    fix

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -74,6 +74,7 @@ class UserController extends BaseController
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $this->userModel
                 ->setPhone($form->get('phone')->getData())
+                ->setEmail($form->get('email')->getData())
                 ->setBirthDate($form->get('birthDate')->getData())
                 ->setRole($form->get('role')->getData())
                 ->setName($form->get('name')->getData())
