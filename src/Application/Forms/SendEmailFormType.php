72988d944a655d5e73befe63af1323cc346ffa71
StereoFlo <StereoFlo@users.noreply.github.com>
2019-03-09 18:18:38 +0300

    forms fix

--- src/Application/Forms/SendEmailFormType.php
+++ src/Application/Forms/SendEmailFormType.php
@@ -3,25 +3,10 @@
 
 namespace Application\Forms;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
-
 /**
  * Class SendEmailFromType
  * @package Application\Forms
  */
-class SendEmailFormType extends AbstractType
+class SendEmailFormType extends AbstractSendMessageFormType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('message', TextareaType::class)
-            ->add('submit', SubmitType::class);
-    }
 }
--- src/Application/Forms/SendSmsFormType.php
+++ src/Application/Forms/SendSmsFormType.php
@@ -2,25 +2,11 @@
 
 namespace Application\Forms;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
-
 /**
  * Class SendSmsFormType
  * @package Application\Forms
  */
-class SendSmsFormType extends AbstractType
+class SendSmsFormType extends AbstractSendMessageFormType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('message', TextareaType::class)
-            ->add('submit', SubmitType::class);
-    }
+
 }
